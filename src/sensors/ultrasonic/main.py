b74c4b1b3c60b0011b1c9d48b49412303ab962ca
gpoell <gpoell.gp@gmail.com>
2024-12-03 12:37:32 -0500

    cleanining up main.py logic and leveraging new client module in Ultrasonic

--- src/sensors/ultrasonic/main.py
+++ src/sensors/ultrasonic/main.py
@@ -1,31 +1,23 @@
 from ultrasonic import Ultrasonic
-from devices import Topic, Publishers, Subscribers, Topics, ClientConfig
+from client.topics import Topics, UltrasonicPublishers, UltrasonicSubscribers
+from client.mqtt_client import ClientConfig
 
 def main():
-    
-    # Define Topics to publish and subscribe
-    publishers = Publishers()
-    publishers.distance = Topic(topic="device/ultrasonic/distance", qos=0)
-    publishers.status = Topic(topic="device/ultrasonic/status", qos=0)
-    publishers.threads = Topic(topic="device/ultrasonic/threads", qos=0)
-    
-    subscribers = Subscribers()
-    subscribers.appStatus = Topic(topic="app/status", qos=1)
 
+    publishers = UltrasonicPublishers()
+    subscribers = UltrasonicSubscribers()
     topics = Topics(publishers, subscribers)
-
     clientConfig = ClientConfig(topics, host="mqtt-broker", port=1883)
-
-    print('Ultrasonic Sensor is initializing ... ')
     ultrasonic = Ultrasonic(clientConfig, trig=27, echo=22, maxDistance=300)
 
     try:
         ultrasonic.run(freq=0.05)
     except KeyboardInterrupt:
         print("Disconnecting...")
-    except ValueError:
-        raise ValueError
+    except ValueError as e:
+        raise ValueError from e
     finally:
         ultrasonic.stop()
+        print("Ultrasonic sensor stopped..")
 
 main()
--- src/sensors/ultrasonic/ultrasonic.py
+++ src/sensors/ultrasonic/ultrasonic.py
@@ -1,16 +1,10 @@
 import time
 import threading
-import asyncio
-import os
 import random
-import RPi.GPIO as GPIO
-import paho.mqtt.client as mqtt
-from enum import IntEnum
-from devices import MqttDevice, ClientConfig
-
-class State(IntEnum):
-    OFF = 0
-    ON = 1
+import importlib
+# import RPi.GPIO as GPIO
+# from devices import MqttDevice, ClientConfig
+from client.mqtt_client import MqttDevice, ClientConfig, Environment, State
 
 
 class Ultrasonic(MqttDevice):
@@ -31,99 +25,142 @@ class Ultrasonic(MqttDevice):
 
         super().__init__(clientConfig)
 
-        self.state = State.OFF
         self.trigger_pin = trig
         self.echo_pin = echo
         self.MAX_DISTANCE = maxDistance         # define the maximum measuring distance, unit: cm
         self.timeOut = self.MAX_DISTANCE * 60   # calculate timeout according to the maximum measuring distance
-        self.client = mqtt.Client()             # Each sensor/module should manage its own client
+        # self.client = mqtt.Client()             # Each sensor/module should manage its own client
+        self.GPIO = None
 
         # GPIO Settings
-        self._set_GPIO()
+        if self.env == Environment.PROD:
+            self._set_GPIO()
 
         # Attach MQTT Event-Based Callbacks to Client
         self.client.on_connect = self.client_on_connect
         self.client.on_message = self.client_on_message
 
 
-    def _set_GPIO(self):
-        """Sets the GPIO pins for the Ultrasonic Sensor."""
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.trigger_pin, GPIO.OUT)
-        GPIO.setup(self.echo_pin, GPIO.IN)
+    def _set_GPIO(self) -> None:
+        """
+        Sets the GPIO pins for the Ultrasonic Sensor when the environment is PROD.
+        The PROD environment indicates the container is running on the Raspberry Pi
+        which is required for importing the RPi.GPIO module.
+        """
+        try:
+            self.GPIO = importlib.import_module("RPi.GPIO")
+            # import RPi.GPIO as GPIO
+            self.GPIO.setwarnings(False)
+            self.GPIO.setmode(self.GPIO.BCM)
+            self.GPIO.setup(self.trigger_pin, self.GPIO.OUT)
+            self.GPIO.setup(self.echo_pin, self.GPIO.IN)
+        except ImportError as e:
+            print(f"Error importing library: {e}")
+            raise e
+        except Exception as e:
+            raise e
 
-    
+
     def pulseIn(self, pin, level, timeOut):  # obtain pulse time of a pin under timeOut
         t0 = time.time()
-        while (GPIO.input(pin) != level):
+        while (self.GPIO.input(pin) != level):
             if ((time.time() - t0) > timeOut * 0.000001):
                 return 0
-        
+
         t0 = time.time()
-        while (GPIO.input(pin) == level):
+        while (self.GPIO.input(pin) == level):
             if ((time.time() - t0) > timeOut * 0.000001):
                 return 0
-        
+
         pulseTime = (time.time() - t0) * 1000000
         return pulseTime
 
 
-    def get_distance(self) -> float:  # get the measurement results of ultrasonic module,with unit: cm
-        distance_cm = [0, 0, 0, 0, 0]
-        for i in range(5):
-            GPIO.output(self.trigger_pin, GPIO.HIGH)  # make trigger_pin output 10us HIGH level
+    def get_distance(self, samples: int=5) -> float:  # get the measurement results of ultrasonic module,with unit: cm
+        """
+        Returns the distance reported by the ultrasonic sensor by applying a median filter on a collection
+        of distance samples to remove noise.
+        """
+
+        # Return mock data if services are not using hardware
+        if self.env == Environment.DEV:
+            distance = random.uniform(120.5, 125.7)
+            print("Publishing distance: ", distance)
+            return distance
+
+        # List comprehension here on new method
+        distance_samples = [0, 0, 0, 0, 0]
+
+        # Abstract this into two functions- 1 should calculate ping time2 should calculate distance
+        for i in range(samples):
+            self.GPIO.output(self.trigger_pin, self.GPIO.HIGH)  # make trigger_pin output 10us HIGH level
             time.sleep(0.00001)  # 10us
-            GPIO.output(self.trigger_pin, GPIO.LOW)  # make trigger_pin output LOW level
-            pingTime = self.pulseIn(self.echo_pin, GPIO.HIGH, self.timeOut)  # read plus time of echo_pin
-            distance_cm[i] = pingTime * 340.0 / 2.0 / 10000.0  # calculate distance with sound speed 340m/s
-        distance_cm = sorted(distance_cm)
-        return distance_cm[2]
+            self.GPIO.output(self.trigger_pin, self.GPIO.LOW)  # make trigger_pin output LOW level
+            pingTime = self.pulseIn(self.echo_pin, self.GPIO.HIGH, self.timeOut)  # read plus time of echo_pin
+            distance_samples[i] = pingTime * 340.0 / 2.0 / 10000.0  # calculate distance with sound speed 340m/s
+        distance_samples = sorted(distance_samples)
+        return distance_samples[2]
 
 
-    def client_on_connect(self, client, userdata, flags, return_code):
-        if return_code == 0:
-            print("Ultrasonic sensor is connected.....")
-        else:
+    def client_on_connect(self, client, userdata, flags, return_code) -> bool:
+        """Client callback when ultrasonic sensor connects to MQTT broker. Temporary placehoder"""
+        if return_code != 0:
             print("could not connect, return code:", return_code)
+            return False
+
+        print("Ultrasonic sensor is connected.....")
+        return True
 
 
     def client_on_message(self, client, userdata, msg):
+        """
+        Event based callback when the ultrasonic sensor receives a message from MQTT broker.
+        The types of messages received by the broker should reflect what is declared in the
+        Subscribers datatype,
+
+        :param client: reference to client instance connected to MQTT broker.
+        :param userdata: <TBD>
+        :param msg: incoming message from MQTT Broker.
+        """
         message = msg.payload.decode()
         topic = msg.topic
-        
+
         if topic == self.subscribers.appStatus and message != "active":
             self.state = State.OFF
 
 
     def run(self, freq: float=0.05):
-        self.status = self._connect_to_broker()
-        environ = os.environ["ENVIRON"]
-        while self.status:
-            time.sleep(freq)
-            distance = self.get_distance() if environ == "PROD" else random.uniform(120.5, 125.7)
-            threadCount = threading.active_count()
+        """
+        Program logic for running the ultrasonic sensor. The sensor continuously publishes distances,
+        its current state, and performance metrics such as the active thread count at a specified
+        sampling rate.
 
-            start_time = time.perf_counter()
-            
+        :param freq: the sampling rate of the ultrasonic sensor.
+        """
+
+        # Connect to MQTT Broker to turn device ON
+        self._connect_to_broker()
+
+        while self.state == State.ON:
+            time.sleep(freq)
+            distance = self.get_distance()
+            thread_count = threading.active_count()
+
             responses = [
                 self.publish_data(self.publishers.distance, distance, timeout=1.0),
-                self.publish_data(self.publishers.threads, threadCount, timeout=1.0),
-                self.publish_data(self.publishers.status, self.status, timeout=1.0),
+                self.publish_data(self.publishers.threads, thread_count, timeout=1.0),
+                self.publish_data(self.publishers.status, self.state, timeout=1.0),
             ]
 
             self.client.publish("device/motor/threads", 8)        # TEMPORARY FOR VISUALIZATION
 
             if False in responses:
-                self.status = False
+                self.state = State.OFF
 
     def stop(self):
-        print("Ultrasonic sensor has been stopped...")
-        self.status = False
-        self.client.publish(
-            topic = self.publishers.status.topic,
-            qos = self.publishers.status.qos,
-            payload = State.OFF
-        )
-        self.client.loop_stop()
-        self.client.disconnect()
+        """
+        This method is used to stop the ultrasonic sensor and is intended to be used to provide functionality
+        in addition to the MqttDevice _disconnect() default method. For example, in the future, it could be
+        possible to keep the sensor running without publishing data to the MQTT broker.
+        """
+        self._disconnect()
