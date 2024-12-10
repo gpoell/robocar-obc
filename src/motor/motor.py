24f2440aebc5caeb137c1e11eb6837ca7f83698b
gpoell <gpoell.gp@gmail.com>
2024-12-10 11:21:34 -0500

    adding motor channel enums for clarity when setting PWM for direction

--- src/motor/motor.py
+++ src/motor/motor.py
@@ -1,13 +1,34 @@
 import math
 import time
+from enum import IntEnum
 from PCA9685 import PCA9685
 from ADC import Adc
+
+class MotorChannels(IntEnum):
+    """
+    PWM channels for vehicle wheels used to determine direction of power supply.
+    """
+    LEFT_UPPER_FORWARD = 0
+    LEFT_UPPER_REVERSE = 1
+    LEFT_LOWER_REVERSE = 2
+    LEFT_LOWER_FORWARD = 3
+    RIGHT_LOWER_FORWARD = 4
+    RIGHT_LOWER_REVERSE = 5
+    RIGHT_UPPER_FORWARD = 6
+    RIGHT_UPPER_REVERSE = 7
+
 
 class Motor:
     """
     This class contains methods for operating the motor of the vehicle (driving forward, backward
     and turning.). It is comprised of the ADC and PCA9685 (PWM) for determining rotation angles and
     supplying power to the DC motors through PWM duty cycles.
+
+    The vehicle uses Differential Steering - a mechanism used to control the speed of independently
+    powered wheels to turn the vehicle. During a turn, the wheels on the outer side of the turn must
+    travel a longer distance than the inner wheels. The differential allows these wheels to rotate
+    at different speeds, ensuring smooth and efficient turning without wheel slippage or strain on
+    the drivetrain.
 
     The vehicle is primarily driven through the set_motor_model which requires positive or negative
     integers representing the PWM duty cycle and direction (positive or negative values). For
@@ -40,6 +61,18 @@ class Motor:
         self.pwm = PCA9685(pwm_address, pwm_frequency)
         self.adc = Adc()
 
+    def drive(self, pwm_duty: int) -> None:
+        """
+        Drives the vehicle forward by supplying consistent power to
+        all 4 wheels of the vehicle through PWM duty cycle ratios.
+        """
+
+        # Future state - PWM ratios should be used instead of exact values
+        # if pwm_duty < 0 or pwm_duty > 1:
+        #     raise ValueError("PWM Duty cycle ratios must be between 0 and 1")
+
+
+
     @staticmethod
     def duty_range(duty1, duty2, duty3, duty4):
         if duty1 > 4095:
@@ -70,16 +103,16 @@ class Motor:
         """
 
         if duty > 0:
-            self.pwm.set_motor_pwm(0, 0)
-            self.pwm.set_motor_pwm(1, duty)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_UPPER_REVERSE, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_UPPER_FORWARD, duty)
 
         elif duty < 0:
-            self.pwm.set_motor_pwm(1, 0)
-            self.pwm.set_motor_pwm(0, abs(duty))
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_UPPER_FORWARD, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_UPPER_REVERSE, abs(duty))
 
         else:
-            self.pwm.set_motor_pwm(0, 0)
-            self.pwm.set_motor_pwm(1, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_UPPER_FORWARD, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_UPPER_REVERSE, 0)
 
 
     def left_lower_wheel(self, duty: int) -> None:
@@ -88,52 +121,53 @@ class Motor:
         """
 
         if duty > 0:
-            self.pwm.set_motor_pwm(3, 0)
-            self.pwm.set_motor_pwm(2, duty)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_LOWER_REVERSE, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_LOWER_FORWARD, duty)
 
         elif duty < 0:
-            self.pwm.set_motor_pwm(2, 0)
-            self.pwm.set_motor_pwm(3, abs(duty))
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_LOWER_FORWARD, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_LOWER_REVERSE, abs(duty))
 
         else:
-            self.pwm.set_motor_pwm(2, 0)
-            self.pwm.set_motor_pwm(3, 0)
-
-
-    def right_upper_wheel(self, duty: int) -> None:
-        """
-        Determines the direction and sets the PWM duty cycle for the Left Upper Wheel
-        """
-
-        if duty > 0:
-            self.pwm.set_motor_pwm(6, 0)
-            self.pwm.set_motor_pwm(7, duty)
-
-        elif duty < 0:
-            self.pwm.set_motor_pwm(7, 0)
-            self.pwm.set_motor_pwm(6, abs(duty))
-
-        else:
-            self.pwm.set_motor_pwm(6, 0)
-            self.pwm.set_motor_pwm(7, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_LOWER_FORWARD, 0)
+            self.pwm.set_motor_pwm(MotorChannels.LEFT_LOWER_REVERSE, 0)
 
 
     def right_lower_wheel(self, duty: int) -> None:
         """
-        Determines the direction and sets the PWM duty cycle for the Left Upper Wheel
+        Determines the direction and sets the PWM duty cycle for the Right Lower Wheel
         """
 
         if duty > 0:
-            self.pwm.set_motor_pwm(4, 0)
-            self.pwm.set_motor_pwm(5, duty)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_LOWER_REVERSE, 0)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_LOWER_FORWARD, duty)
 
         elif duty < 0:
-            self.pwm.set_motor_pwm(5, 0)
-            self.pwm.set_motor_pwm(4, abs(duty))
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_LOWER_FORWARD, 0)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_LOWER_REVERSE, abs(duty))
 
         else:
-            self.pwm.set_motor_pwm(4, 0)
-            self.pwm.set_motor_pwm(5, 0)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_LOWER_REVERSE, 0)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_LOWER_FORWARD, 0)
+
+
+    def right_upper_wheel(self, duty: int) -> None:
+        """
+        Determines the direction and sets the PWM duty cycle for the Right Upper Wheel
+        """
+
+        if duty > 0:
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_UPPER_REVERSE, 0)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_UPPER_FORWARD, duty)
+
+        elif duty < 0:
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_UPPER_FORWARD, 0)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_UPPER_REVERSE, abs(duty))
+
+        else:
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_UPPER_REVERSE, 0)
+            self.pwm.set_motor_pwm(MotorChannels.RIGHT_UPPER_FORWARD, 0)
+
 
 
     def set_motor_model(self, duty1: int, duty2: int, duty3: int, duty4: int) -> None:
@@ -185,13 +219,13 @@ class Motor:
 if __name__ == '__main__':
     motor = Motor()
     try:
-        motor.set_motor_model(-2000, -2000, -2000, -2000)  # Forward
+        motor.set_motor_model(2000, 2000, 2000, 2000)  # Forward
         time.sleep(1)
-        motor.set_motor_model(2000, 2000, 2000, 2000)  # Back
+        motor.set_motor_model(-2000, -2000, -2000, -2000)  # Back
         time.sleep(1)
-        motor.set_motor_model(-4000, -4000, 2000, 2000)  # Right
+        motor.set_motor_model(4000, 4000, 50, 50)  # Right
         time.sleep(1)
-        motor.set_motor_model(4000, 4000, -2000, -2000)  # Left
+        motor.set_motor_model(50, 50, 4000, 4000)  # Left
         time.sleep(2)
         motor.stop()    # Stop
 
