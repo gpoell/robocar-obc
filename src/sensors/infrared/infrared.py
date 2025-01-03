482f7f81d97e13b80a28ad196202028cc3d11905
Raspi4 <gpoell.gp@gmail.com>
2025-01-03 17:09:46 -0500

    adding infrared container and POC of lane detection

--- src/sensors/infrared/infrared.py
+++ src/sensors/infrared/infrared.py
@@ -3,6 +3,7 @@ import importlib
 from client.mqtt_client import MqttDevice, ClientConfig, Environment, State
 from enum import IntEnum
 from signal import pause
+from time import sleep
 
 
 
@@ -18,7 +19,7 @@ class IRPins(IntEnum):
 class LineDetector(MqttDevice):
     """
     The LineDetector is positioned at the bottom of the vehicle and is used
-    to detect when the vehicle crosses a line. It is composed of 3 infrared sensors
+    to detect when the vehicle crosses a line. It is composed of 3 infrared (IR) sensors
     that emit infrared light and detect the intensity of the light reflected back to
     the sensor, which is particularly useful for detecting lanes on contrasting surfaces
     to support autonomous lane navigation.
@@ -29,36 +30,47 @@ class LineDetector(MqttDevice):
 
     PARAMETERS:
 
+    :param client_config:   The MQTT client configuration for subscribing/publishing data
     :param samples:         The amount of samples each sensor collects. Default is 5.
     :param sample_rate:     The number of samples to read per second. Default is 100.
-    :param threshold:       The threshold for determining an "active" state. Default is 0.5.
+    :param threshold:       The threshold for determining an "active" state based on sample majority. Default is 0.5.
+    :param frequency:       The frequency for sampling data from each IR sensor. Default is 20 HZ.
     """
 
     def __init__(
         self,
-        client_config: ClientConfig,
+        # client_config: ClientConfig,
         samples: int = 5,
         sample_rate: int = 100,
-        threshold: float = 0.5
+        threshold: float = 0.5,
+        frequency: float = 0.05
     ) -> None:
 
+        # Brief Type and Value Checks for major issues
         if not isinstance(samples, int):
             raise TypeError("Supported types for samples are: <int>")
         if not isinstance(sample_rate, int):
             raise TypeError("Supported types for sample_rates are: <int>")
         if not isinstance(threshold, float):
             raise TypeError("Supported types for threshold are: <float>")
-        if not isinstance(client_config, ClientConfig):
-            raise TypeError("Supported types for client_config are: <ClientConfig>")
+        # if not isinstance(client_config, ClientConfig):
+        #     raise TypeError("Supported types for client_config are: <ClientConfig>")
+        if not isinstance(frequency, float):
+            raise TypeError("Supported types for frequency are: <float>")
         if samples < 1 or sample_rate < 1:
             raise ValueError("Samples and sample rates must be positive values.")
         if 1 < threshold < 0:
             raise ValueError("Thresholds to determine active states must be between 0 and 1.")
+        if frequency < 0:
+            raise ValueError("Frequency values must be positive.")
 
         # Initialize MqttDevice
-        super().__init__(client_config)
+        # super().__init__(client_config)
+
+        self.frequency = frequency
 
         # Import gpiozero if running on Raspberry Pi
+        self.env = Environment.PROD
         gpiozero = self.__import_gpiozero()
 
 
@@ -85,29 +97,17 @@ class LineDetector(MqttDevice):
             raise e
 
 
-    def line_detected(self) -> None:
-        print(f"Line detected on {self.__class__.__name__}", self.value, sep=": ")
-
-    def line_not_detected(self) -> None:
-        print(f"Line not detected on {self.__class__.__name__}", self.value, sep=": ")
-
-
     def run(self) -> None:
-        self.IR01.when_line = self.line_detected
-        self.IR02.when_line = self.line_detected
-        self.IR03.when_line = self.line_detected
-
-        self.IR03.when_no_line = self.line_not_detected
-        self.IR03.when_no_line = self.line_not_detected
-        self.IR03.when_no_line = self.line_not_detected
-
-        pause()
+        while True: # Change to sensor state
+            sleep(self.frequency)
+            print(self.IR01.value, self.IR02.value, self.IR03.value, sep='|')
 
 if __name__ == '__main__':
 
     sensor = LineDetector()
 
     try:
+        print("Sensor is running...")
         sensor.run()
     except KeyboardInterrupt:
         print("Sensor is stopping...")
